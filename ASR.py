6412f574b2d0079c38d74f72c31a8e8b777d1915
MathisLB <mathis.leboite@gmail.com>
2025-03-19 13:49:01 +0100

    Add transcription output print for ASR model

--- ASR.py
+++ ASR.py
@@ -1,3 +1,5 @@
 from speechbrain.inference.ASR import EncoderDecoderASR
+
 asr_model = EncoderDecoderASR.from_hparams(source="speechbrain/asr-crdnn-rnnlm-librispeech", savedir="./pretrained_ASR", hparams_file="hyperparams.yaml")
-asr_model.transcribe_file("./LibriSpeech/dev-clean-2/1272/135031/1272-135031-0003.flac")
+transcription = asr_model.transcribe_file("./LibriSpeech/dev-clean-2/1272/135031/1272-135031-0003.flac")
+print(transcription)
