710076b390a22a6bde93ca74fc0f765c973fa77d
MathisLB <mathis.leboite@gmail.com>
2025-03-19 17:54:04 +0100

    Add PCA visualization for speaker embeddings in diarization

--- diarization.py
+++ diarization.py
@@ -1,7 +1,36 @@
 from speechbrain.inference.speaker import SpeakerRecognition
 import torchaudio
+import glob
+import numpy as np
+from pathlib import Path
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
+
 verification = SpeakerRecognition.from_hparams(source="speechbrain/spkrec-ecapa-voxceleb", savedir="./pretrained_ecapa")
-signal, fs = torchaudio.load('./LibriSpeech/dev-clean-2/1272/135031/1272-135031-0003.flac')
-embedding = verification.encode_batch(signal)
-embedding.shape
-print(embedding)
+
+utterances = glob.glob("./LibriSpeech/dev-clean-2/**/*.flac", recursive=True)
+
+np.random.shuffle(utterances)
+utterances = utterances[:20]
+
+embeddings = []
+labels = []
+for u in utterances:
+    tmp, fs = torchaudio.load(u)
+    e = verification.encode_batch(tmp)
+    embeddings.append(e[0, 0].numpy())
+    spk_label = Path(u).parent.parent.stem
+    labels.append(spk_label)
+
+embeddings = np.array(embeddings)
+
+pca = PCA(n_components=2)
+principalComponents = pca.fit_transform(embeddings)
+
+fig, ax = plt.subplots()
+ax.scatter(principalComponents[:, 0], principalComponents[:, 1])
+
+for i, spkid in enumerate(labels):
+    ax.annotate(spkid, (principalComponents[i, 0], principalComponents[i, 1]))
+plt.xlabel("Principal Component 1")
+plt.ylabel("Principal Component 2")
