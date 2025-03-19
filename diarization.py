b01223b69ffb4abbf8e11201642bb80f19258ed7
MathisLB <mathis.leboite@gmail.com>
2025-03-19 13:57:55 +0100

    Add speaker recognition functionality

---
+++ diarization.py
@@ -0,0 +1,6 @@
+from speechbrain.inference.speaker import SpeakerRecognition
+import torchaudio
+verification = SpeakerRecognition.from_hparams(source="speechbrain/spkrec-ecapa-voxceleb", savedir="./pretrained_ecapa")
+signal, fs = torchaudio.load('./LibriSpeech/dev-clean-2/1272/135031/1272-135031-0003.flac')
+embedding = verification.encode_batch(signal)
+embedding.shape
