e5b915c3c669006876677497c3edee20bfb3877b
MathisLB <mathis.leboite@gmail.com>
2025-03-19 14:37:37 +0100

    Add ECAPA model and hyperparameters for speaker verification

--- diarization.py
+++ diarization.py
@@ -4,3 +4,4 @@ verification = SpeakerRecognition.from_hparams(source="speechbrain/spkrec-ecapa-
 signal, fs = torchaudio.load('./LibriSpeech/dev-clean-2/1272/135031/1272-135031-0003.flac')
 embedding = verification.encode_batch(signal)
 embedding.shape
+print(embedding)
