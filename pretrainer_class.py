ae0d0eb026a7d7f2e3ba024ce14422457e648d92
MathisLB <mathis.leboite@gmail.com>
2025-03-20 11:32:30 +0100

    Add pretrainer initialization and model loading in pretrainer_class.py

--- pretrainer_class.py
+++ pretrainer_class.py
@@ -1,4 +1,5 @@
 from speechbrain.lobes.models.ECAPA_TDNN import ECAPA_TDNN
+from speechbrain.utils.parameter_transfer import Pretrainer
 
 model = ECAPA_TDNN(input_size= 80,
                    channels= [1024, 1024, 1024, 1024, 3072],
@@ -6,3 +7,10 @@ model = ECAPA_TDNN(input_size= 80,
                    dilations= [1, 2, 3, 4, 1],
                    attention_channels= 128,
                    lin_neurons = 192)
+
+# Initialization of the pre-trainer
+pretrain = Pretrainer(loadables={'model': model}, paths={'model': 'speechbrain/spkrec-ecapa-voxceleb/embedding_model.ckpt'})
+
+# We download the pretrained model from HuggingFace in this case
+pretrain.collect_files()
+pretrain.load_collected()
