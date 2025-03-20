29936c2a7035ab8637c413e00481600a1e9fe49d
MathisLB <mathis.leboite@gmail.com>
2025-03-20 09:28:29 +0100

    Add checking of configurations before training

---
+++ view_params.py
@@ -0,0 +1,8 @@
+from speechbrain.inference.ASR import EncoderDecoderASR
+asr_model = EncoderDecoderASR.from_hparams(source="speechbrain/asr-crdnn-rnnlm-librispeech", savedir="./pretrained_ASR", hparams_file="hyperparams.yaml")
+
+print(asr_model.mods.keys())
+
+print(asr_model.mods.encoder)
+
+print(asr_model.mods.encoder.compute_features)
